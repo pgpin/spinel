3eacb127feb232b9be490c75b96e8a1d192ae1f1
James McGill <jbmcgill@gmail.com>
2018-12-10 14:37:00 -0800

    default 1024 random byte secret if not provided in configuration file

--- src/spinel/config.go
+++ src/spinel/config.go
@@ -1,31 +1,45 @@
 package spinel
 
 import "gopkg.in/yaml.v2"
+import "github.com/adam-hanna/randomstrings"
 
 type ConfigAd struct {
-	Host string
-	Port int
-	Dn  string
+	Host                 string
+	Port                 int
+	Dn                   string
 	MaxRequestsPerSecond int
 }
 
-type Html struct{
-  LoginTitle string
+type ConfigHtml struct {
+	Templates  string
+	LoginTitle string
 }
 
 type Config struct {
-	Listen string
-	Secret string
-	Ad     ConfigAd
-	Cidrs  []string
-	Html Html
+	Debug   bool
+	Socket  string
+	Expires int64
+	Secret  string
+	Ad      ConfigAd
+	Cidrs   []string
+	Html    ConfigHtml
 }
 
 func ParseYamlConfiguration(yamlstr *[]byte) (*Config, error) {
-	var config Config
-	err := yaml.Unmarshal([]byte(*yamlstr), &config)
+	secret, _ := randomstrings.GenerateRandomString(1024)
+	config := &Config{Debug: false,
+		Socket:  "/tmp/spinel.sock",
+		Expires: 24,
+		Secret:  secret,
+		Ad: ConfigAd{Host: "localhost",
+			Port:                 389,
+			Dn:                   "dc=CORP",
+			MaxRequestsPerSecond: 10},
+		Html: ConfigHtml{LoginTitle: "Login with ActiveDirectory credentials",
+			Templates: "/usr/local/share/spinel/tmpl"}}
+	err := yaml.Unmarshal([]byte(*yamlstr), config)
 	if err != nil {
 		return nil, err
 	}
-	return &config, nil
+	return config, nil
 }
